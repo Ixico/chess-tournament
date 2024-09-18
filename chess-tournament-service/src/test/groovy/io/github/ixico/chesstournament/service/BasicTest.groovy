ab89fd2b4951d15baaee9ed96e87e2e204759056
Ixico <01159054@pw.edu.pl>
2024-09-18 23:06:52 +0200

    test config

---
+++ chess-tournament-service/src/test/groovy/io/github/ixico/chesstournament/service/BasicTest.groovy
@@ -0,0 +1,11 @@
+package io.github.ixico.chesstournament.service
+
+import spock.lang.Specification
+
+class BasicTest extends Specification {
+
+    def "context loads"() {
+        expect:
+            1+1==2
+    }
+}
